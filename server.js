7e622c2bae81d7fea93a3259f0564dd5a3d62ea5
MatthiasL <matthias_ludwig@yahoo.com>
2015-05-24 09:37:14 +1000

    01 Hello world

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var app = express();
 
 app.use( express.static( __dirname + '/public' ) );
 
-if (process !== null){
+if (typeof process.env.C9_HOSTNAME !== 'undefined') {
   console.log('Your code is running at https://' + process.env.C9_HOSTNAME);
   app.listen(process.env.PORT, process.env.IP);  
-}else{
+} else {
+  console.log('Your code is running at http://localhost:3000');
   app.listen(3000);  
 }
