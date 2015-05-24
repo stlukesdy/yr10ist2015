61a0ca9a0da26a06b6891bc34b4801b1327646b6
MatthiasL <matthias_ludwig@yahoo.com>
2015-05-24 17:07:31 +1000

    03 done

---
+++ public/04_Multi-way_selection/program.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+  $("#runbutton").click(program);
+});
+
+var program = function() {
+
+
+  var myGuess = 42;
+
+  var guess = $("#guess").val();
+  guess = parseInt(guess);
+
+  if (guess === myGuess) {
+    console.log("Well done - you guessed my number!");
+  } else {
+    console.log("That is not correct.");
+  }
+
+};
