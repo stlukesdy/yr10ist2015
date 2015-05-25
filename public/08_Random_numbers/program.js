6308c3dd8ca1f81dcf3d007169e478aa55ac0c5c
MatthiasL <matthias_ludwig@yahoo.com>
2015-05-26 06:34:27 +1000

    08 Random numbers

--- public/08_Random_numbers/program.js
+++ public/08_Random_numbers/program.js
@@ -3,9 +3,6 @@
 });
 
 var program = function() {
-
-  for (var i = 1; i < 13; i = i + 1) {
-    console.log('12 x ' + i + ' = ' + (i * 12));
-  }
-
+  var r = Math.floor((Math.random() * 10) + 1);
+  console.log(r);
 };
