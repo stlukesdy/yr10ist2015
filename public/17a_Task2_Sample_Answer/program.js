dacfe192bd244f13de570ffe84434383b3f13353
MatthiasL <matthias_ludwig@yahoo.com>
2015-06-10 09:42:50 +1000

    17a Task2 Sample Answer

---
+++ public/17a_Task2_Sample_Answer/program.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+  $("#runButton").click(program);
+});
+
+var program = function () {
+  var amount = $("#amountInput").val();
+  amount = parseInt(amount);
+
+  var discount = '';
+
+  if (amount > 25) {
+    if (amount >= 80) {
+      discount = '30%';
+    } else {
+      discount = '20%';
+    }
+  } else {
+    discount = '10%';
+  }
+
+  $("#outputP").html(discount);
+  console.log("The result is " + discount);
+};
